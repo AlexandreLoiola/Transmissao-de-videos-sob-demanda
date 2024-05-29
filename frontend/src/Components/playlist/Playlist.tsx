c250e62e87cc314c66d7b686084be008ebab6815
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-05-28 22:44:06 -0300

    feat: create playlist view

---
+++ frontend/src/Components/playlist/Playlist.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyledContainer, StyledImage, StyledItem, StyledText, StyledTitle } from "./styles";
+import { IVideo } from "../../interfaces/IVideo";
+
+interface PlaylistProps {
+  videos: IVideo[];
+  onVideoSelect: (video: IVideo) => void;
+}
+
+const Playlist: React.FC<PlaylistProps> = ({ videos, onVideoSelect }) => {
+  return (
+    <StyledContainer>
+      <StyledText>Próximos Vídeos</StyledText>
+      {videos.map((video, index) => (
+        <StyledItem key={index} onClick={() => onVideoSelect(video)}>
+          <StyledImage src={video.thumbnailUrl} alt={`Thumbnail ${index + 1}`} />
+          <StyledTitle>{video.title}</StyledTitle>
+        </StyledItem>
+      ))}
+    </StyledContainer>
+  );
+};
+export default Playlist;
---
+++ frontend/src/Components/playlist/styles.ts
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  padding-top: 10px;
+  width: 25vw;
+  position: fixed;
+  right: 0;
+  background-color: #F9F9F9;
+  box-shadow: 0px 0px 20px 0px #0000001A;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  overflow-y: auto;
+`;
+
+export const StyledItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  transition: transform 0.3s ease-in-out;
+  
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const StyledImage = styled.img`
+  height: 180px;
+  width: 280px;
+  cursor: pointer;
+  border-radius: 12px;
+`;
+
+export const StyledTitle = styled.span`
+  margin-top: 4px;
+  margin-bottom: 20px;
+  font-family: Roboto;
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 24px;
+  text-align: center;
+  color: #000000CC;
+`;
+
+export const StyledText = styled.h1`
+  margin-top: 32px;
+  margin-bottom: 30px;
+  font-family: Roboto;
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 24px;
+  text-align: center;
+  color: #000000CC;
+`;
