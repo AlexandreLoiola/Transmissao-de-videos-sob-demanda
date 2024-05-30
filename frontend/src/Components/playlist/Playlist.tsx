5418c456eba2339aba33422b2f9891bf2b70a0f8
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-05-30 10:09:15 -0300

    feat: Add red line to playlist thumbnails

--- frontend/src/Components/playlist/Playlist.tsx
+++ frontend/src/Components/playlist/Playlist.tsx
@@ -5,15 +5,19 @@ import { IVideo } from "../../interfaces/IVideo";
 interface PlaylistProps {
   videos: IVideo[];
   onVideoSelect: (video: IVideo) => void;
+  currentVideo: IVideo;
 }
 
-const Playlist: React.FC<PlaylistProps> = ({ videos, onVideoSelect }) => {
+const Playlist: React.FC<PlaylistProps> = ({ videos, onVideoSelect, currentVideo }) => {
   return (
     <StyledContainer>
       <StyledText>Próximos Vídeos</StyledText>
       {videos.map((video, index) => (
-        <StyledItem key={index} onClick={() => onVideoSelect(video)}>
-          <StyledImage src={video.thumbnailUrl} alt={`Thumbnail ${index + 1}`} />
+        <StyledItem 
+          key={index} 
+          onClick={() => onVideoSelect(video)} 
+        >
+          <StyledImage src={video.thumbnailUrl} alt={`Thumbnail ${index + 1}`} style={{ border: video.videoUrl === currentVideo.videoUrl ? '4px solid red' : 'none' }} />
           <StyledTitle>{video.title}</StyledTitle>
         </StyledItem>
       ))}
